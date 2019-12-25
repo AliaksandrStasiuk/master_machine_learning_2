e89b19c596c48e833875fc9de763ef4bc7130ef0
Stasiuk-InData <stasiuk-indata@Stasiuk-InDatas-MacBook-Pro.local>
2019-12-25 14:24:48 +0300

    Init commit for 2.

---
+++ 2/config.py
@@ -0,0 +1,8 @@
+dataset_url = 'https://commondatastorage.googleapis.com/books1000/notMNIST_small.tar.gz'
+
+
+image_size = 28  # Pixel width and height.
+pixel_depth = 255.0 # Number of levels per pixel.
+
+train_percentage = 0.8
+test_percentage = 0.2
